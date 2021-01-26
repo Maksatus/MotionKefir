fe60d76095d3d8ae037e03a704c000bcdd0fcc3c
Max <streetworkout042000@gmail.com>
2021-01-26 19:42:38 +0300

    up

---
+++ Assets/playerGravity.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class playerGravity : MonoBehaviour
+{
+    private CharacterController controller;
+    private float gravityValue = -9.81f;
+    private bool groundedPlayer;
+    private Vector3 playerVelocity;
+
+    private void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
